f1959b234e3e39bde03a962c9726d79c36fc5c81
DameerGamlet <gubaidulin.dam@gmail.com>
2025-01-15 18:59:12 +0400

    feat: adding keycloak service

---
+++ api-gateway/src/main/java/com/inventory/demo/configuration/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.inventory.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder.withJwkSetUri("http://localhost:8181/realms/spring-microservices-security-realm/protocol/openid-connect/certs")
+//                .build();
+//    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(a -> a.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
