c526cf2f1ade7146eb050d4a00cd987241c58c5a
DameerGamlet <gubaidulin.dam@gmail.com>
2025-01-17 14:57:22 +0400

    feat: adding ExceptionHandler

--- order-service/src/main/java/com/order/demo/configuration/RestClientConfig.java
+++ order-service/src/main/java/com/order/demo/configuration/RestClientConfig.java
@@ -2,11 +2,16 @@ package com.order.demo.configuration;
 
 import com.order.demo.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,10 +23,19 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         final RestClient client = RestClient.builder()
                 .baseUrl(INVENTORY_URL)
+                .requestFactory(getClientRequestFactory())
                 .build();
 
         var adapter = RestClientAdapter.create(client);
         var proxyFactory = HttpServiceProxyFactory.builderFor(adapter).build();
         return proxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        final ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.defaults()
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+
+        return ClientHttpRequestFactoryBuilder.detect().build(settings);
     }
 }
